29916d3ac36841b00ed96a3c0b361a14f85c4c6b
Henrique Pettenuci <riquepet09@gmail.com>
2021-04-15 20:16:03 -0300

    feat: change output log to json

---
+++ src/common/Logger.py
@@ -0,0 +1,30 @@
+import logging
+
+from jsonformatter import JsonFormatter
+
+def loggingOverride(name: str,logLevel=logging.INFO):
+    STRING_FORMAT = '''{
+        "datetime":        "asctime",
+        "levelName":       "levelname",
+        "module":          "module",
+        "lineNumber":      "lineno",            
+        "process":         "process",
+        "message":         "message"
+    }'''
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)    
+    formatter = JsonFormatter(
+        STRING_FORMAT,
+        mix_extra=True,
+        mix_extra_position='tail'
+    )
+
+
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    sh.setLevel(logLevel)
+
+    logger.addHandler(sh)
+
+    return logger
