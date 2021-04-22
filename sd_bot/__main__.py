f6e5af40dfc1ddb02f49f17a8f07d55b4a611ddb
Henrique Pettenuci <hpettenuci@icloud.com>
2021-04-22 00:49:03 -0300

    fix: improve smoke tests

--- sd_bot/__main__.py
+++ sd_bot/__main__.py
@@ -2,4 +2,5 @@ from sd_bot.sd_bot import DiscordBot
 
 if __name__ == "__main__":
     discordbot = DiscordBot()
-    discordbot.startBot()
+    if discordbot.DISCORD_TOKEN != "SMOKE":
+        discordbot.startBot()
